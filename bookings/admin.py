6854ea789aa735595e0d984ae0ec56c3337d8175
Edna Torres Munill <ethra822@gmail.com>
2024-09-19 11:17:15 +0000

    styled worlds page, and ordered booking list display in admin

--- bookings/admin.py
+++ bookings/admin.py
@@ -2,4 +2,8 @@
 from django.contrib import admin
 from .models import Booking
 
-admin.site.register(Booking)
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('date', 'world','start_time', 'user', 'id',)
+
+admin.site.register(Booking, BookingAdmin)
--- bookings/forms.py
+++ bookings/forms.py
@@ -1,10 +1,20 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
 from .models import Booking
 
+
 class BookingForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(BookingForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Book Now'))
+
     class Meta:
         model = Booking
         exclude = ['user']
         widgets = {
-            'booking_date': forms.DateInput(attrs={'type': 'date'}),
+            'date': forms.DateInput(attrs={'type': 'date'}),
         }
