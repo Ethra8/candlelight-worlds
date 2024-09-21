3cb25fc9ee22b2cc51d0cc4ebf46953415050035
Edna Torres Munill <ethra822@gmail.com>
2024-09-21 18:28:16 +0000

    style contact form

--- bookings/models.py
+++ bookings/models.py
@@ -4,15 +4,15 @@ from cloudinary.models import CloudinaryField
 
 
 TIME_SLOTS = [
-    ('10:00', '10:00 AM - 04:00 PM'),
-    ('18:00', '06:00 PM - 08:00 AM'),
+    ('10:00', '10:00 AM - 05:00 PM'),
+    ('18:00', '07:00 PM - 08:00 AM'),
 ]
 
 class Booking(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    world = models.ForeignKey('worlds.World', on_delete=models.CASCADE, default=1)
     date = models.DateField()
     start_time = models.CharField(max_length=5, choices=TIME_SLOTS)
-    world = models.ForeignKey('worlds.World', on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return f'Booking {self.id} - {self.user.username} - {self.world.display_name} - {self.date} - {self.get_start_time_display()}'
